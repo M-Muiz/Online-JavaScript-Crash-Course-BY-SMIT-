fd52e827259dfcb7b194b1f39b003326d5249836
M-Muiz <122803576+M-Muiz@users.noreply.github.com>
2023-04-07 05:29:46 +0500

    Add files via upload

---
+++ Assigment 1/java/app.js
@@ -0,0 +1,128 @@
+// Question:---
+
+/*
+let age = +prompt("Enter Your Age");
+
+if(age <= 10){
+    alert("Welcome!");
+}else{
+    alert("You are not allowed!");
+}
+*/
+
+// Question:--
+
+/*
+let table = +prompt("Enter a number");
+
+for(var i = 1; i <=10; i++){
+    console.log(table + " x " + i + " = " + table*i);
+}
+*/
+
+// Question: 1
+
+/*
+let a = +prompt("Enter num 1");
+let b = +prompt("Enter num 2");
+
+if(a > b){
+    alert("The greater number is " + a);
+}else if(a < b){
+    alert("The greater number is " + b);
+}else if(a == b){
+    alert("Both numbers are equal! " + a + "=" + b);
+}else{
+    alert("Plz! Enter only a number");
+}
+*/
+
+// Question: 2
+
+/*
+let sign = +prompt("Enter your number");
+
+if(sign < 0 ){
+    alert("The sign is - ");
+}else{
+    alert("The sign is + ");
+}
+*/
+
+// Question: 3
+
+/*
+let a = +prompt("Enter number 1");
+let b = +prompt("Enter number 2");
+let c = +prompt("Enter number 3");
+let d = +prompt("Enter number 4");
+let e = +prompt("Enter number 5");
+
+let result = Math.max(a,b,c,d,e);
+alert("The Largest Number is " + result)
+*/
+
+
+// Question: 4
+
+/*
+for (var i = 0; i<=15; i++){
+    if(i % 2 == 1){
+        console.log(i + " is Odd ");
+    }else{
+        console.log(i + " is Even ");
+    }
+}
+*/
+
+// Question: 5
+
+/*
+let percent = +prompt("Enter Your Percentage");
+
+if(percent > 90 && percent < 100){
+    alert("Your Grade is A");
+}else if(percent > 80 && percent < 90){
+    alert("Your Grade is B");
+}else if(percent > 70 && percent < 80){
+    alert("Your Grade is C");
+}else if(percent > 60 && percent < 70){
+    alert("Your Grade is D");
+}else if(percent > 20 && percent < 60){
+    alert("Your Grade is F");
+}else{
+   alert("Not Valid! Percentage");
+}
+*/
+
+
+// Question: 6
+
+/*
+for(var i= 1; i<=100; i++){
+  if(i %3 == 0 && i % 5 == 0){
+        console.log(i + " Fizz Buzz");
+      }
+  else if(i %3 == 0){
+     console.log(i + " Fizz");
+  }else if(i % 5 == 0){
+    console.log(i + " Buzz");
+  }
+else{
+    console.log(i + " Not Dividable");
+}
+}
+*/
+
+// Question: 7
+
+/*
+for(var i=1; i<=5; i++){
+    var star ="";
+    for(var j=1; j<= i; j++){
+    star = star + "* ";
+    }
+    console.log(star)
+}
+*/
+
