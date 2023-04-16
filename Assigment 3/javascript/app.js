b6d14762b0d1a38853f017b5f74c8d6e2b5a6052
M-Muiz <122803576+M-Muiz@users.noreply.github.com>
2023-04-16 23:42:58 +0500

    Assigment 3

---
+++ Assigment 3/javascript/app.js
@@ -0,0 +1,6 @@
+function result(){
+    let a  = document.getElementById("genre").value;
+    let b  = document.getElementById("year").value;
+    let c  = document.getElementById("language").value;
+    let d  = document.getElementById("rating").value;
+}
